3f47242df7499edf34710190951a202912e4a88b
Kian <137164508+Kianhossini@users.noreply.github.com>
2025-06-28 23:16:39 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,47 @@
 #!/bin/sh
 
-# configs
-AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
-ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
-PORT=8080
+configs
 
-# setup
-mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
-wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip CONFIGCADDY=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/Caddyfile CONFIGXRAY=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/xray.json ParameterSSENCYPT=chacha20-ietf-poly1305 StoreFiles=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/StoreFiles PORT=8080
 
-# storefiles
-mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+prepare filesystem
+
+mkdir -p /etc/caddy/ /usr/share/caddy && 
+echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+
+download files
+
+wget $CADDYIndexPage -O /tmp/index.zip && 
+unzip -qo /tmp/index.zip -d /tmp/web && 
+mv /tmp/web// /usr/share/caddy/
+
+wget -qO- $CONFIGCADDY | 
+sed -e "1c :$PORT" 
+-e "s/$AUUID/$AUUID/g" 
+-e "s/$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" 
+>/etc/caddy/Caddyfile
+
+wget -qO- $CONFIGXRAY | 
+sed -e "s/$AUUID/$AUUID/g" 
+-e "s/$ParameterSSENCYPT/$ParameterSSENCYPT/g" 
+>/xray.json
+
+fetch and prepare store files
+
+mkdir -p /usr/share/caddy/$AUUID && 
+wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles && 
 wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
 
-for file in $(ls /usr/share/caddy/$AUUID); do
-    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
-done
+generate download page
 
-# start services
+for file in $(ls /usr/share/caddy/$AUUID); do [ "$file" != "StoreFiles" ] && 
+echo "<a href="$file" download>$file</a><br>" >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html done
+
+start services
+
 tor &
+
 /xray -config /xray.json &
+
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
