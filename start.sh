76b91761566b9e9f02f771a1bf1e42ecf61d334e
Kian <137164508+Kianhossini@users.noreply.github.com>
2025-06-28 20:46:05 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,9 @@ CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Cadd
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
-#PORT=4433
+PORT=8080
+
+# setup
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
@@ -21,9 +23,7 @@ for file in $(ls /usr/share/caddy/$AUUID); do
     [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
 done
 
-# start
+# start services
 tor &
-
 /xray -config /xray.json &
-
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
