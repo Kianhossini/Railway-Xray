9839a05bbde46dbfb82bafafd3128c439b50f584
Kian <137164508+Kianhossini@users.noreply.github.com>
2025-06-28 23:36:37 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,46 +1,49 @@
 #!/bin/sh
 
-# configs
+# تنظیمات اولیه
 AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/xray.json
-ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Kianhossini/Railway-Xray/main/etc/StoreFiles
 PORT=8080
+PARAM=chacha20-ietf-poly1305
 
-# prepare filesystem
-mkdir -p /etc/caddy/ /usr/share/caddy && \
-echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+# دانلود فایل‌ها
+mkdir -p /etc/caddy /usr/share/caddy /etc/xray
 
-# download files
-wget $CADDYIndexPage -O /tmp/index.zip && \
-unzip -qo /tmp/index.zip -d /tmp/web && \
-mv /tmp/web/*/* /usr/share/caddy/
+# محتوای ساده HTML
+echo "Xray Running - Powered by Railway" > /usr/share/caddy/index.html
 
-wget -qO- $CONFIGCADDY | \
-sed -e "1c :$PORT" \
-    -e "s/\$AUUID/$AUUID/g" \
-    -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" \
-    >/etc/caddy/Caddyfile
+# Caddyfile
+cat <<EOF >/etc/caddy/Caddyfile
+:$PORT
+root * /usr/share/caddy
+file_server
+reverse_proxy /$AUUID-vmess localhost:10000
+EOF
 
-wget -qO- $CONFIGXRAY | \
-sed -e "s/\$AUUID/$AUUID/g" \
-    -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" \
-    >/xray.json
+# پیکربندی Xray
+cat <<EOF >/etc/xray/config.json
+{
+  "inbounds": [{
+    "port": 10000,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [{
+        "id": "$AUUID"
+      }]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+        "path": "/$AUUID-vmess"
+      }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom"
+  }]
+}
+EOF
 
-# fetch and prepare store files
-mkdir -p /usr/share/caddy/$AUUID && \
-wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles && \
-wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
-
-# generate download page
-for file in $(ls /usr/share/caddy/$AUUID); do
-  [ "$file" != "StoreFiles" ] && \
-  echo "<a href=\"$file\" download>$file</a><br>" >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
-done
-
-# start services
+# اجرای سرویس‌ها
 tor &
-/xray -config /xray.json &
+xray -config /etc/xray/config.json &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
